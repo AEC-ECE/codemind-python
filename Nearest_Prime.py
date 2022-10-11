bcd8346608006113de1d1fdcd68a38aa8215190e
Saisravani naidu <84873983+AEC-ECE@users.noreply.github.com>
2022-10-11 15:14:24 +0530

    t=int(input()) for i in range(t):     n=int(input())     next_prime=n     while True:         fc=0         for j in range(1,next_prime+1):             if next_prime%j==0:                 fc+=1         if fc==2:             break         next_prime+=1     p_prime=n     while True:         fc=0         for p in range(1,p_prime+1):             if p_prime%p==0:                 fc+=1         if fc==2:             break         p_prime-=1     if n - p_prime <= next_prime - n:         print(p_prime)     else:         print(next_prime)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -4,24 +4,22 @@ for i in range(t):
     next_prime=n
     while True:
         fc=0
-        for j in range (1,next_prime+1):
+        for j in range(1,next_prime+1):
             if next_prime%j==0:
                 fc+=1
         if fc==2:
             break
         next_prime+=1
-       
     p_prime=n
     while True:
         fc=0
-        for s in range(1,p_prime+1):
-            if p_prime%s==0:
+        for p in range(1,p_prime+1):
+            if p_prime%p==0:
                 fc+=1
         if fc==2:
             break
         p_prime-=1
-        
-    if n-p_prime<=next_prime-n:
+    if n - p_prime <= next_prime - n:
         print(p_prime)
     else:
         print(next_prime)
